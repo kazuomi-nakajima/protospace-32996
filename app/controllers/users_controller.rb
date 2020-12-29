c7323bcbfd9bdf37d57baf2c72abd04a9d816a45
kazuomi-nakajima <x15076.nakajima@gmail.com>
2020-12-29 15:36:56 +0900

    ユーザー名リンクの遷移先+ユーザー詳細ページの表示内容修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @prototypes = Prototype.all.includes(:user)
+    @prototypes = @user.prototypes
   end
 end
